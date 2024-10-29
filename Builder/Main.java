1320cf633d504f7f873011b5ecb1f48bd630245e
Tuyen Tran <s1f102000051@iniad.org>
2024-10-29 22:18:56 +0900

    Simplfied Example Builder

--- Builder/Main.java
+++ Builder/Main.java
@@ -1,20 +1,20 @@
 package Builder;
 
 // Client code to use the Builder Pattern
+// Client code to use the Builder Pattern
 public class Main {
     public static void main(String[] args) {
-        // Computer gamingComputer = new Computer.Builder("Intel i7", "NVIDIA RTX 3080")
-        //         .setRAM(32)
-        //         .setStorage(1000)
-        //         .setBluetoothEnabled(true)
-        //         .setWiFiEnabled(true)
-        //         .build();
-
-        // System.out.println(gamingComputer);
-
-        // Computer basicComputer = new Computer.Builder("AMD Ryzen 5", "Integrated GPU")
+        // Creating a computer with default RAM
+        // Computer basicComputer = new Computer.Builder("Intel i5")
         //         .build();
 
         // System.out.println(basicComputer);
+
+        // // Creating a computer with custom RAM
+        // Computer highEndComputer = new Computer.Builder("Intel i7")
+        //         .setRAM(16)
+        //         .build();
+
+        // System.out.println(highEndComputer);
     }
 }
